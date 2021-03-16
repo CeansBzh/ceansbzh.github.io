5ae65512b5d9dcb49e5c7798f72dbaac49636f23
Ceansbzh <ceansbzh@gmail.com>
2021-03-16 18:54:29 +0100

    Enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './vue-tailwind.settings.js', 'node_modules/vue-tailwind/dist/*.js'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
