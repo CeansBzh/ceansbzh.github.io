f772c00e6bab9e7277157d420ea50d4e475a697e
Ceansbzh <ceansbzh@gmail.com>
2021-03-16 18:05:15 +0100

    Fix bug not finding vue-tailwind settings

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueTailwind from 'vue-tailwind'
-import VueTailwindSettings from './vue-tailwind.settings.js';
+import VueTailwindSettings from '../vue-tailwind.settings.js';
 import './index.css'
 
 createApp(App).use(router).use(VueTailwind, VueTailwindSettings).mount('#app')
