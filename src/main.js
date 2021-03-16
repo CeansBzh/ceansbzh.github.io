0cae56cfd1f62a87fd4e8c874ced84f2fc96b705
Ceansbzh <ceansbzh@gmail.com>
2021-03-16 18:01:57 +0100

    Add vue-tailwind

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueTailwind from 'vue-tailwind'
+import VueTailwindSettings from './vue-tailwind.settings.js';
 import './index.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueTailwind, VueTailwindSettings).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './vue-tailwind.settings.js', 'node_modules/vue-tailwind/dist/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
