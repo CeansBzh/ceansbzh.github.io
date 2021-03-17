b1c698d5ad3332ad84f3f1ead63ff8214a95c501
Ceansbzh <ceansbzh@gmail.com>
2021-03-17 12:31:16 +0100

    Improve year responsiveness

--- src/components/EducationElement.vue
+++ src/components/EducationElement.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="flex md:contents" :class="{ 'flex-row-reverse': side }">
-        <div v-if="!side" class="transform -rotate-90 mb:rotate-0 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto flex items-end">
+        <div v-if="!side" class="hidden md:flex col-start-1 col-end-5 rounded-xl ml-auto md:-mr-6 my-4 items-center">
             <h4 class="text-lg text-gray-900 dark:text-gray-200">{{ year }}</h4>
         </div>
-        <div v-else class="bg-green-700 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-            <h3 class="font-bold text-xl mb-1">{{ title }}</h3>
+        <div v-else class="bg-green-700 col-start-1 col-end-5 p-4 rounded-xl my-4 mr-auto md:mr-0 md:ml-auto shadow-md">
+            <h3 class="font-bold text-xl">{{ title }}</h3>
+            <p class="pb-2 text-gray-200 text-sm md:hidden">
+                {{ year }}
+            </p>
             <p class="leading-tight text-justify mb-4">
                 {{ description }}
             </p>
@@ -22,7 +25,10 @@
             <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow"></div>
         </div>
         <div v-if="!side" class="bg-green-700 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-            <h3 class="font-bold text-xl mb-1">{{ title }}</h3>
+            <h3 class="font-bold text-xl">{{ title }}</h3>
+            <p class="pb-2 text-gray-200 text-sm md:hidden">
+                {{ year }}
+            </p>
             <p class="leading-tight text-justify mb-4">
                 {{ description }}
             </p>
@@ -33,7 +39,7 @@
                 {{ adress }}
             </p>
         </div>
-        <div v-else class="hidden md:flex col-start-6 col-end-10 p-4 rounded-xl -ml-10 items-center">
+        <div v-else class="hidden md:flex col-start-6 col-end-10 rounded-xl mr-auto md:-ml-6 my-4 items-center">
             <h4 class="text-lg text-gray-900 dark:text-gray-200">{{ year }}</h4>
         </div>
     </div>
