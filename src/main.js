1bdaf4a0a2bf279ad0354a6c535459f854346bb1
Ceansbzh <ceansbzh@gmail.com>
2021-03-19 17:31:39 +0100

    Add socials link with tippy popover

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import VueTippy from 'vue-tippy'
+import 'tippy.js/dist/tippy.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueTippy, {
+  directive: 'tippy', // => v-tippy
+  component: 'tippy', // => <tippy/>
+  componentSingleton: 'tippy-singleton', // => <tippy-singleton/>
+  defaultProps: { placement: 'left' },
+}).mount('#app')
