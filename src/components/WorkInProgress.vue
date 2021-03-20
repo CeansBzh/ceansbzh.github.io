e673e2c56ab0b65d605a204ca0caf9a019341d89
Ceansbzh <ceansbzh@gmail.com>
2021-03-20 12:49:05 +0100

    Add WIP placeholder

---
+++ src/components/WorkInProgress.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mx-4 p-5 text-center text-gray-900 dark:text-white">
+        <h1 class="font-bold text-6xl mb-4">En cours de création</h1>
+        <h2 class="font-bold text-3xl mb-12 text-gray-900 dark:text-white text-opacity-80 dark:text-opacity-75">Revenez plus tard !</h2>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
