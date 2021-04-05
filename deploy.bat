06f434fa21315bfb6113a1243f9645d92584c0ce
Ceansbzh <ceansbzh@gmail.com>
2021-04-05 23:09:04 +0200

    Improve script

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
-npm run build
-cd dist
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:CeansBzh/CeansBzh.github.io.git main:gh-pages
+call npm run build
+call cd dist
+call git init
+call git add -A
+call git commit -m 'deploy'
+call git push -f git@github.com:CeansBzh/CeansBzh.github.io.git main:gh-pages
