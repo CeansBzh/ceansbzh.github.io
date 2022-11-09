a5d31e128f48c0686d38cb62a2330fa04c906232
CeansBzh <ebriantais@orange.fr>
2022-11-09 10:44:58 +0100

    Update my education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -7,25 +7,21 @@
                     <div class="h-10 w-1 bg-gradient-to-b from-transparent to-green-700 pointer-events-none"></div>
                 </div>
             </div>
-            <!-- left -->
-            <EducationElement
-                title="DUT Informatique"
-                description="Formation en cours à l'IUT de Vannes."
-                establishment="IUT de Vannes"
-                website="https://www.iutvannes.fr/"
-                adress="8 Rue Michel de Montaigne, 56000 Vannes"
-                year="2020 - 2022 (en cours)"
-                side
-            ></EducationElement>
             <!-- right -->
-            <EducationElement
-                title="Bac Scientifique"
+            <EducationElement title="Licence Informatique"
+                description="Formation en cours."
+                establishment="ISTIC, Université de Rennes 1" website="https://istic.univ-rennes1.fr/"
+                adress="Bâtiment 12 D - 263 avenue du Général Leclerc, 35042 Rennes" year="2022 - 2023"></EducationElement>
+            <!-- left -->
+            <EducationElement title="DUT Informatique"
+                description="2ème année en parcours développeur. Obtenu avec un avis très favorable du jury concernant ma poursuite d'étude."
+                establishment="IUT de Vannes" website="https://www.iutvannes.fr/"
+                adress="8 Rue Michel de Montaigne, 56000 Vannes" year="2020 - 2022" side></EducationElement>
+            <!-- right -->
+            <EducationElement title="Bac Scientifique"
                 description="Option informatique et sciences du numérique. Obtenu mention bien."
-                establishment="Lycée René Descartes"
-                website="https://www.lycee-descartes.fr/"
-                adress="14 Chemin de Ronde CS 20705, 35207 Rennes"
-                year="2020"
-            ></EducationElement>
+                establishment="Lycée René Descartes" website="https://www.lycee-descartes.fr/"
+                adress="14 Chemin de Ronde CS 20705, 35207 Rennes" year="2020"></EducationElement>
             <!-- fade out of the timeline bar -->
             <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
                 <div class="h-full w-6 flex items-center justify-center">
@@ -46,4 +42,5 @@ export default {
 </script>
 
 <style>
+
 </style>
