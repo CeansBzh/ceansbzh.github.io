7fa58af333e8e0e92b59af1aba610145f5877b28
CeansBzh <ebriantais@orange.fr>
2023-01-24 20:42:57 +0100

    Add extra small (xs) tailwind breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /* eslint-env node */
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
