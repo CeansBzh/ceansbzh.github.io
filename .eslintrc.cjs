abd3af3284e1603383283d237629c3889acf0bd8
CeansBzh <ebriantais@orange.fr>
2023-01-24 18:54:54 +0100

    Install Tailwindcss (+ plugins for eslint and prettier)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,17 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  plugins: ["tailwindcss"],
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:tailwindcss/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  rules: {
+    "tailwindcss/no-custom-classname": "off", // override recommended rule
   },
 };
