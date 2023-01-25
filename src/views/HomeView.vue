b041bc17046c5d5e7972cac772a8970464a9d9c1
CeansBzh <ebriantais@orange.fr>
2023-01-25 09:53:15 +0100

    Make welcome text actually look good

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,15 @@ import TheProjects from "../components/TheProjects.vue";
 
 <template>
   <main>
-    <p>Je m'appelle</p>
-    <h1>Elouan Briantais</h1>
-    <p>
-      et je suis un étudiant en informatique, aspirant développeur web backend
-    </p>
+    <div
+      class="mx-auto mt-12 mb-24 max-w-xl sm:my-24 lg:my-40 xl:my-48 2xl:my-56 2xl:max-w-2xl"
+    >
+      <p class="mb-3 text-2xl text-gray-400">Bonjour 👋</p>
+      <p class="text-2xl text-gray-400">Je m'appelle</p>
+      <h1 class="mb-3 text-5xl text-gray-100">Elouan Briantais</h1>
+      <p class="text-2xl text-gray-400">et je suis</p>
+      <p class="text-3xl text-gray-100">Étudiant en informatique à l'ISTIC</p>
+    </div>
 
     <TheProjects />
   </main>
