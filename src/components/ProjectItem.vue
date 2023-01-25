9ea642d295e2c381417b752d4a51f6fbdb63a2d4
CeansBzh <ebriantais@orange.fr>
2023-01-25 18:51:55 +0100

    Add project component and first project (Akipaj)

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AppLink from "./AppLink.vue";
+
+defineProps({
+  route: String,
+  color: String,
+});
+</script>
+
+<template>
+  <article
+    class="group mx-auto flex w-fit flex-col items-center justify-center rounded-2xl sm:flex-row sm:grayscale sm:transition sm:duration-500 sm:hover:grayscale-0"
+  >
+    <h2
+      class="-mb-4 mr-auto transition duration-500 after:absolute after:bottom-3 after:left-0 after:-z-10 after:h-2 after:w-full after:origin-bottom-right after:scale-x-0 after:transition-transform after:duration-500 after:ease-out after:content-[''] group-hover:-translate-y-5 group-hover:after:origin-bottom-left group-hover:after:scale-x-100 sm:-mr-14 sm:after:bottom-8 sm:after:h-3 sm:group-hover:translate-y-0 sm:group-hover:-translate-x-20 md:-mr-28 md:group-hover:-translate-x-32"
+      :class="color"
+    >
+      <AppLink
+        :to="route"
+        class="text-right text-[5rem] font-bold text-neutral-400/50 transition duration-500 group-hover:text-neutral-200 sm:text-[6rem] xl:text-[10rem]"
+        aria-label="Voir le projet"
+      >
+        <slot name="name"></slot>
+      </AppLink>
+    </h2>
+    <AppLink :to="route" aria-label="Voir le projet">
+      <slot name="image"></slot>
+    </AppLink>
+  </article>
+</template>
--- src/components/TheProjects.vue
+++ src/components/TheProjects.vue
@@ -1,5 +1,30 @@
+<script setup>
+import ProjectItem from "./ProjectItem.vue";
+</script>
+
 <template>
-  <main>
-    <h1>Projets</h1>
-  </main>
+  <section class="mb-52">
+    <div class="p-5">
+      <div class="mx-auto max-w-2xl">
+        <h1 class="mb-8 font-bold uppercase text-neutral-300">Projets</h1>
+      </div>
+
+      <ul class="mx-auto max-w-6xl">
+        <li>
+          <ProjectItem route="https://akipaj.fr" color="after:bg-[#0ea5e9]">
+            <template #name>Akipaj</template>
+            <template #image>
+              <img
+                alt="Image du projet Akipaj"
+                src="@/assets/projects/akipaj.jpg"
+                width="560"
+                height="498"
+                class="w-full max-w-xs select-none rounded-2xl sm:w-56 md:w-auto md:max-w-xs lg:max-w-sm xl:max-w-md"
+              />
+            </template>
+          </ProjectItem>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
