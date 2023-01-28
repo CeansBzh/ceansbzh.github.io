a9418301cfe7553effa41fea0d7994fd8ad0bb6c
CeansBzh <ebriantais@orange.fr>
2023-01-28 11:17:26 +0100

    Remove base css file

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -15,7 +15,7 @@ defineProps({
     class="group mx-auto flex w-fit flex-col items-center justify-center rounded-2xl sm:grayscale sm:transition sm:duration-500 sm:hover:grayscale-0"
   >
     <h2
-      class="-mb-4 mr-auto transition duration-500 after:absolute after:bottom-3 after:left-0 after:-z-10 after:h-2 after:w-full after:origin-bottom-right after:scale-x-0 after:transition-transform after:duration-500 after:ease-out after:content-[''] group-hover:-translate-y-5 group-hover:after:origin-bottom-left group-hover:after:scale-x-100 sm:after:bottom-8 sm:after:h-3 sm:group-hover:translate-y-0"
+      class="-mb-4 mr-auto transition duration-500 after:absolute after:bottom-3 after:left-0 after:-z-10 after:h-2 after:w-full after:origin-bottom-right after:scale-x-0 after:transition-transform after:duration-500 after:ease-out after:content-[''] group-hover:-translate-y-5 group-hover:after:origin-bottom-left group-hover:after:scale-x-100 sm:after:-bottom-4 sm:after:h-3 sm:group-hover:translate-y-0"
       :class="color"
     >
       <AppLink
