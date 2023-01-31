fd8d61c2ad5dbc5e8331caa7a84be06c271707b3
CeansBzh <ebriantais@orange.fr>
2023-01-31 14:26:58 +0100

    Refactor project images directory structure

--- src/components/TheProjects.vue
+++ src/components/TheProjects.vue
@@ -15,12 +15,12 @@ import ProjectItem from "./ProjectItem.vue";
         class="mx-auto max-w-6xl [&>li:not(:last-child)]:mb-20 [&>li:nth-child(even)_article]:sm:flex-row-reverse [&>li:nth-child(even)_article]:sm:space-x-reverse [&>li:nth-child(odd)_article]:sm:flex-row [&>li:nth-child(odd)_article>h2]:sm:translate-x-20 [&>li:nth-child(even)_article>h2]:sm:-translate-x-20 [&>li:nth-child(even)_article:hover>h2]:sm:translate-x-0 [&>li:nth-child(odd)_article:hover>h2]:sm:translate-x-0 [&>li_article]:sm:space-x-5"
       >
         <li>
-          <ProjectItem route="https://akipaj.fr" color="after:bg-[#0ea5e9]">
+          <ProjectItem route="/projets/akipaj" color="after:bg-[#0ea5e9]">
             <template #name>Akipaj</template>
             <template #image>
               <img
                 alt="Image du projet Akipaj"
-                src="@/assets/projects/akipaj.jpg"
+                src="@/assets/projects/akipaj/project-item.jpg"
                 width="448"
                 height="398"
                 class="w-full max-w-xs select-none rounded-2xl sm:w-56 md:w-auto md:max-w-xs lg:max-w-sm xl:max-w-md"
@@ -34,7 +34,7 @@ import ProjectItem from "./ProjectItem.vue";
             <template #image>
               <img
                 alt="Image du projet Rhiven"
-                src="@/assets/projects/rhiven.jpg"
+                src="@/assets/projects/rhiven/project-item.jpg"
                 width="448"
                 height="398"
                 class="w-full max-w-xs select-none rounded-2xl sm:w-56 md:w-auto md:max-w-xs lg:max-w-sm xl:max-w-md"
