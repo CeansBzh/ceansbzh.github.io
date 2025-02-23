8a859c6d1fb8c31b9760e7fb796d2132519195a8
CeansBzh <ebriantais@orange.fr>
2025-02-23 22:09:08 +0100

    Updates

---
+++ assets/zen-config-Ci3fQFTe.js
@@ -0,0 +1,17 @@
+const n=`# Configuration navigateur [Zen](https://zen-browser.app/)
+
+Inspiration : [Zanca.dev](https://zanca.dev/notes/Zen-Browser-Config)
+
+## \`about:config\`
+
+| **Paramètre**                      | **Valeur** | **But**                                 |
+| ---------------------------------- | ---------- | --------------------------------------- |
+| \`browser.tabs.loadBookmarksInTabs\` | true       | Ouvre les favoris dans un nouvel onglet |
+
+## Raccourcis clavier
+
+| **Raccourci**       | **Valeur** |
+| ------------------- | ---------- |
+| Save page           | _Not set_  |
+| Toggle Compact Mode | \`ctrl+s\`   |
+`;export{n as default};
