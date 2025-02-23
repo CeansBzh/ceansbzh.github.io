fc970cd2c041365d4d3e58f900e802589bbaecca
CeansBzh <ebriantais@orange.fr>
2025-02-23 22:10:40 +0100

    Retire le css oublié et corrige le script de déploiement qui s'exécute 2 fois

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -22,37 +22,3 @@ watch(
 <template>
   <div id="markdown" ref="content"></div>
 </template>
-
-<style scoped>
-h1 {
-  @apply text-4xl font-bold mb-4;
-}
-
-p {
-  @apply text-base mb-4;
-}
-
-h2 {
-  @apply text-3xl font-semibold mb-3;
-}
-
-code {
-  @apply bg-gray-800 text-white p-2 rounded-md;
-}
-
-table {
-  @apply table-auto w-full border-collapse mb-4;
-}
-
-th {
-  @apply border px-4 py-2 text-left bg-gray-200;
-}
-
-td {
-  @apply border px-4 py-2;
-}
-
-em {
-  @apply italic;
-}
-</style>
